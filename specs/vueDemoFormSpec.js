760b2d8a8b0a2e697b65ab27933e48ccf7064a38
Anri Zeneli <azeneli46@gmail.com>
2020-10-12 14:53:45 -0400

    positive and negative test specs

--- specs/vueDemoFormSpec.js
+++ specs/vueDemoFormSpec.js
@@ -1,3 +1,4 @@
+import basePage from '../pages/basePage';
 import Chance from 'chance';
 
 const chance = new Chance();
@@ -21,8 +22,6 @@ describe('Vue Demo From Test Suite', () => {
 			await browser.sleep(3000);
 
 			const result = firstNameEl.getAttribute('text');
-			console.log(result);
-
 			expect(result).toBe(firstName);
 		});
 
@@ -34,10 +33,7 @@ describe('Vue Demo From Test Suite', () => {
 			await browser.sleep(3000);
 
 			const result = lastNameEl.getAttribute('text');
-			console.log(result);
-
 			expect(result).toBe(lastName);
-
 		});
 
 		it('should allow me to enter an Email address', async () => {
@@ -48,8 +44,6 @@ describe('Vue Demo From Test Suite', () => {
 			await browser.sleep(3000);
 
 			const result = emailEl.getAttribute('email');
-			console.log(result);
-
 			expect(result).toBe(emailEl);
 		});
 
@@ -88,6 +82,72 @@ describe('Vue Demo From Test Suite', () => {
 
 			expect(successSubmissionEl.isPresent()).toBe(true);
 		});
+	});	
+
+	describe('Tests with in-valid data entered', () => {
+
+		it('should navigate to the form url', async () => {
+			const url = 'https://teroauralinna.github.io/vue-demo-form/';
+			browser.get(url);
+			await browser.sleep(3000);
+
+			expect(browser.getCurrentUrl()).toBe(url);
+		});
+
+		it('should not allow me to proceed with No Data Entered', async () => {
+			const submitButton = $('button[type="submit"]');
+			submitButton.click();
+
+			await browser.sleep(3000);
+			const alertDangerEl = $('div.alert.alert-danger');
+
+			expect(alertDangerEl.isPresent()).toBe(true);
+		});
+
+		it('should not proceed if you did Not Complete all the forms', async () => {
+//			browser.get('https://teroauralinna.github.io/vue-demo-form/');
+			await browser.sleep(3000);
+
+			const firstName = chance.first();
+			const firstNameEl = $('#firstName');
+			firstNameEl.sendKeys(firstName);
+
+			await browser.sleep(3000);
+
+			const lastName = chance.last();
+			const lastNameEl = $('#lastName');	
+			lastNameEl.sendKeys(lastName);
+
+			const email = chance.email();
+			const emailEl = $('#email');
+			emailEl.sendKeys(email);
+
+
+			const submitButton = $('button[type="submit"]');
+			submitButton.click();
+			await browser.sleep(3000);
+
+			const alertDangerEl = $('div.alert.alert-danger');
+			expect(alertDangerEl.isPresent()).toBe(true);
+		});
+
+
+		it('should not allow me to proceed with an Invalid Email entered', async () => {
+			await browser.refresh(3000);
+
+			const emailEl = $('#email');
+			const emailText = 'asdfasffdafdsfa@';
+			emailEl.sendKeys(emailText);
+
+			const submitButton = $('button[type="submit"]');
+			submitButton.click();
+			await browser.sleep(3000);
+
+			const invalidEmailWarning = $('#email div.invalid-feedback');
+			expect(invalidEmailWarning.isPresent()).toBe(true);
+		});
+		
+
 	});	
 
 });
