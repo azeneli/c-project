cd55a05f1094a552129e9ed6e86711271ee9b438
Anri Zeneli <azeneli46@gmail.com>
2020-10-12 22:04:04 -0400

    refactor using methods

--- specs/vueDemoFormSpec.js
+++ specs/vueDemoFormSpec.js
@@ -6,11 +6,6 @@ const chance = new Chance();
 describe('Vue Demo From Test Suite', () => {
 
 	describe('Tests with valid data entered', () => {
-
-		// beforeEach(async () => {
-		// 	await vueFormPage.goto();
-		// });
-
 		it('should navigate to the form url', async () => {
 			await vueFormPage.goto();
 			const url = 'https://teroauralinna.github.io/vue-demo-form/'
@@ -21,9 +16,8 @@ describe('Vue Demo From Test Suite', () => {
 		it('should allow me to enter First name', async () => {
 			const firstName = chance.first();
 			const firstNameEl = $('#firstName');
-
-			firstNameEl.sendKeys(firstName);
-			await browser.sleep(3000);
+			await browser.sleep(2000);
+			vueFormPage.fillInput(firstNameEl, firstName);
 
 			const result = firstNameEl.getAttribute('text');
 			expect(result).toBe(firstName);
@@ -32,8 +26,8 @@ describe('Vue Demo From Test Suite', () => {
 		it('should allow me to enter Last name', async () => {
 			const lastName = chance.last();
 			const lastNameEl = $('#lastName');
+			vueFormPage.fillInput(lastNameEl, lastName);
 
-			lastNameEl.sendKeys(lastName);
 			await browser.sleep(3000);
 
 			const result = lastNameEl.getAttribute('text');
@@ -43,8 +37,8 @@ describe('Vue Demo From Test Suite', () => {
 		it('should allow me to enter an Email address', async () => {
 			const email = chance.email();
 			const emailEl = $('#email');
-			
-			emailEl.sendKeys(email);
+			vueFormPage.fillInput(emailEl, email);
+		
 			await browser.sleep(3000);
 
 			const result = emailEl.getAttribute('email');
@@ -70,8 +64,7 @@ describe('Vue Demo From Test Suite', () => {
 
 		it('should allow me to check the Terms & Conditions', async () => {
 			const checkBoxEl = $('#terms');
-			checkBoxEl.click();
-			await browser.sleep(3000);
+			vueFormPage.waitAndClick(checkBoxEl);
 
 			expect(checkBoxEl.isSelected()).toBe(true);
 		});
@@ -79,52 +72,47 @@ describe('Vue Demo From Test Suite', () => {
 
 		it('should allow me to Submit the from with the required fields completed', async () => {
 			const submitButton = $('button[type="submit"]');
-			submitButton.click();
-
+			vueFormPage.waitAndClick(submitButton);
+			
 			await browser.sleep(3000);
-			const successSubmissionEl = $('div.alert.alert-success');
 
+			const successSubmissionEl = $('div.alert.alert-success');
 			expect(successSubmissionEl.isPresent()).toBe(true);
 		});
 	});	
 
-	xdescribe('Tests with in-valid data entered', () => {
+	describe('Tests with in-valid data entered', () => {
 
-		it('should navigate to the form url', async () => {
-			const url = 'https://teroauralinna.github.io/vue-demo-form/';
-			browser.get(url);
-			await browser.sleep(3000);
-
-			expect(browser.getCurrentUrl()).toBe(url);
-		});
+		beforeAll(async () => {
+			await vueFormPage.goto();
+		})
 
 		it('should not allow me to proceed with No Data Entered', async () => {
 			const submitButton = $('button[type="submit"]');
-			submitButton.click();
+			vueFormPage.waitAndClick(submitButton);
 
 			await browser.sleep(3000);
-			const alertDangerEl = $('div.alert.alert-danger');
 
+			const alertDangerEl = $('div.alert.alert-danger');
 			expect(alertDangerEl.isPresent()).toBe(true);
 		});
 
 		it('should not proceed if you did Not Complete all the forms', async () => {
-//			browser.get('https://teroauralinna.github.io/vue-demo-form/');
 			await browser.sleep(3000);
 
 			const firstName = chance.first();
 			const firstNameEl = $('#firstName');
-			firstNameEl.sendKeys(firstName);
+			vueFormPage.fillInput(firstNameEl, firstName);
 
 			await browser.sleep(3000);
 
 			const lastName = chance.last();
 			const lastNameEl = $('#lastName');	
-			lastNameEl.sendKeys(lastName);
+			vueFormPage.fillInput(lastNameEl, lastName);
 
 			const email = chance.email();
 			const emailEl = $('#email');
-			emailEl.sendKeys(email);
+			vueFormPage.fillInput(emailEl, email);
 
 
 			const submitButton = $('button[type="submit"]');
@@ -134,14 +122,13 @@ describe('Vue Demo From Test Suite', () => {
 			const alertDangerEl = $('div.alert.alert-danger');
 			expect(alertDangerEl.isPresent()).toBe(true);
 		});
-
 
 		it('should not allow me to proceed with an Invalid Email entered', async () => {
 			await browser.refresh(3000);
 
 			const emailEl = $('#email');
 			const emailText = 'asdfasffdafdsfa@';
-			emailEl.sendKeys(emailText);
+			vueFormPage.fillInput(emailEl, email);
 
 			const submitButton = $('button[type="submit"]');
 			submitButton.click();
