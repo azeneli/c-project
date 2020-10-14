0c77206fddd49ebe6afac5d30c48a26cdda33f26
Anri Zeneli <azeneli46@gmail.com>
2020-10-13 22:27:52 -0400

    added comments

--- pages/basePage.js
+++ pages/basePage.js
@@ -15,11 +15,23 @@ export default class BasePage {
 
 	}
 
+    /**
+     * wait and verify that a page is loaded
+     * @returns {promise}
+     * @requires a page to include `pageLoaded` method
+     */
+
     async loaded() {
         return browser.wait(async () => {
             return await this.pageLoaded();
         }, this.timeout.xl, 'timeout: waiting for page to load. The url is: ' + this.url);
     }
+
+    /**
+     * navigate to a page via it's `url` var
+     * and verify/wait via loaded()
+     * @requires page have both `url` and `pageLoaded` properties
+     */
 
 	async goto() {
 		try {
@@ -30,7 +42,10 @@ export default class BasePage {
 		}
 	}
 
-	// write a function that takes the el and send the keys
+    /**
+     * fill texts in the element that is selected
+     * @requires both `el` and `text` params supplied
+     */
 	async fillInput(el, text) {
 		try {
 			el.sendKeys(text);
@@ -39,12 +54,19 @@ export default class BasePage {
 		}	
 	}
 
-	// use this for clicking el
+	/**
+     * wait and then click an element
+     * @param  {obj} element
+     */
 	async waitAndClick(element) {
         await this.isClickable(element);
         await element.click();
     }
 
+    /**
+     * Wrapper(s) for expected conditions
+     * @returns {ExpectedCondition}
+     */
 
     isClickable(locator) {
         return protractor.ExpectedConditions.elementToBeClickable(locator);
